8e6fa9bfc700e2d6322749c56b8e95621ad5702e
krolyxon <krolyxon@tutanota.com>
2022-06-05 02:00:02 +0530

    nvim-tree fix

--- lua/plugins/configs/cmp.lua
+++ lua/plugins/configs/cmp.lua
@@ -84,8 +84,8 @@ local options = {
       }),
    },
    sources = {
-      { name = "nvim_lsp" },
       { name = "luasnip" },
+      { name = "nvim_lsp" },
       { name = "buffer" },
       { name = "nvim_lua" },
       { name = "path" },
