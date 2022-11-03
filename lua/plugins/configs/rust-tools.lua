f8b65ffd4772f89859b9bd03e8a70bbed7e5ffe3
krolyxon <krolyxon@tutanota.com>
2022-11-03 21:22:07 +0530

    fix rust-tools default values

--- lua/plugins/configs/rust-tools.lua
+++ lua/plugins/configs/rust-tools.lua
@@ -4,7 +4,7 @@ if not present then
 end
 
 local capabilities = vim.lsp.protocol.make_client_capabilities()
-capabilities = require("cmp_nvim_lsp").update_capabilities(capabilities)
+capabilities = require("cmp_nvim_lsp").default_capabilities(capabilities)
 
 
 local options = {
