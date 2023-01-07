db2dbd82d192064001a592f1083c475d37e8b23f
krolyxon <krolyxon@tutanota.com>
2023-01-07 13:47:24 +0530

    config(cmp): simplify disabling scrollbar

--- lua/plugins/configs/cmp.lua
+++ lua/plugins/configs/cmp.lua
@@ -21,20 +21,12 @@ local function border(hl_name)
     }
 end
 
-local cmp_window = require "cmp.utils.window"
-
-cmp_window.info_ = cmp_window.info
-cmp_window.info = function(self)
-    local info = self:info_()
-    info.scrollable = false
-    return info
-end
-
 local options = {
     window = {
         completion = {
             border = border "CmpBorder",
             winhighlight = "Normal:CmpPmenu,CursorLine:PmenuSel,Search:None",
+            scrollbar = false,
         },
         documentation = {
             border = border "CmpDocBorder",
