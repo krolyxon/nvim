2df4b6565ddfe7c84e55eda9545f6ca46b494039
krolyxon <krolyxon@tutanota.com>
2023-01-20 00:27:47 +0530

    use default mapping for diagnostic goto_next

--- lua/core/mappings.lua
+++ lua/core/mappings.lua
@@ -248,7 +248,7 @@ M.lspconfig = {
             "goto prev",
         },
 
-        ["d]"] = {
+        ["]d"] = {
             function()
                 vim.diagnostic.goto_next()
             end,
