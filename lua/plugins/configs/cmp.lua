7e8bf345242eac795ff7684ec6b106f1c9c7fa07
krolyxon <krolyxon@tutanota.com>
2023-01-26 00:15:34 +0530

    cmp: fix typo in config

--- lua/plugins/configs/cmp.lua
+++ lua/plugins/configs/cmp.lua
@@ -92,7 +92,7 @@ local options = {
         },
         ["<Tab>"] = cmp.mapping(function(fallback)
             if cmp.visible() then
-                cmp.select_next_item({behavior = ' elect'})
+                cmp.select_next_item()
             elseif require("luasnip").expand_or_jumpable() then
                 vim.fn.feedkeys(vim.api.nvim_replace_termcodes("<Plug>luasnip-expand-or-jump", true, true, true), "")
             else
