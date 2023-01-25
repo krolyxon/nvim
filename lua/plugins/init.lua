1d777e68c7a206459b3543b1f477db9385a3affb
krolyxon <krolyxon@tutanota.com>
2023-01-25 23:51:22 +0530

    fix crates.nvim breaking cmp

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -99,11 +99,8 @@ local plugins = {
         event = { "BufRead Cargo.toml" },
         config = function()
             require("plugins.configs.others").crates()
-        end,
-        -- Add crates.nvim to the cmp sources as soon as we load the plugin
-        init = function()
-            local cmp = require("cmp");
-            cmp.setup.buffer({ sources = { { name = "crates" } } })
+            -- Add crates.nvim to the cmp sources as soon as we load the plugin
+            require("cmp").setup.buffer({ sources = { { name = "crates" } } })
         end,
     },
 
