e5dbcbaaff3967cb5cf9ffa6828179d4b88d74d0
krolxon <krolyxon@tutanota.com>
2023-12-30 22:17:18 +0530

    lspconfig: jdtls

--- lua/plugins/configs/lspconfig.lua
+++ lua/plugins/configs/lspconfig.lua
@@ -152,6 +152,34 @@ lspconfig.asm_lsp.setup {
 
 }
 
+lspconfig.jdtls.setup {
+    on_attach = M.on_attach,
+    capabilities = M.capabilities,
+    init_option = {
+        provideFormatter = true,
+        init_options = {
+            extendedClientCapabilities = {
+                classFileContentsSupport = true,
+                generateToStringPromptSupport = true,
+                hashCodeEqualsPromptSupport = true,
+                advancedExtractRefactoringSupport = true,
+                advancedOrganizeImportsSupport = true,
+                generateConstructorsPromptSupport = true,
+                generateDelegateMethodsPromptSupport = true,
+                moveRefactoringSupport = true,
+                overrideMethodsPromptSupport = true,
+                executeClientCommandSupport = true,
+                inferSelectionSupport = {
+                    'extractMethod',
+                    'extractVariable',
+                    'extractConstant',
+                    'extractVariableAllOccurrence',
+                },
+            },
+        },
+    },
+}
+
 local servers = {
     "marksman",
     "taplo",
