308808026bc74728dc35a19e0998bbc70db317e3
krolxon <krolyxon@tutanota.com>
2024-05-01 01:22:00 +0530

    add todo-comments plugin

--- lua/plugins/configs/lspconfig.lua
+++ lua/plugins/configs/lspconfig.lua
@@ -158,24 +158,24 @@ lspconfig.jdtls.setup {
     init_option = {
         provideFormatter = true,
         init_options = {
-            extendedClientCapabilities = {
-                classFileContentsSupport = true,
-                generateToStringPromptSupport = true,
-                hashCodeEqualsPromptSupport = true,
-                advancedExtractRefactoringSupport = true,
-                advancedOrganizeImportsSupport = true,
-                generateConstructorsPromptSupport = true,
-                generateDelegateMethodsPromptSupport = true,
-                moveRefactoringSupport = true,
-                overrideMethodsPromptSupport = true,
-                executeClientCommandSupport = true,
-                inferSelectionSupport = {
-                    'extractMethod',
-                    'extractVariable',
-                    'extractConstant',
-                    'extractVariableAllOccurrence',
-                },
-            },
+            -- extendedClientCapabilities = {
+            --     classFileContentsSupport = true,
+            --     generateToStringPromptSupport = true,
+            --     hashCodeEqualsPromptSupport = true,
+            --     advancedExtractRefactoringSupport = true,
+            --     advancedOrganizeImportsSupport = true,
+            --     generateConstructorsPromptSupport = true,
+            --     generateDelegateMethodsPromptSupport = true,
+            --     moveRefactoringSupport = true,
+            --     overrideMethodsPromptSupport = true,
+            --     executeClientCommandSupport = true,
+            --     inferSelectionSupport = {
+            --         'extractMethod',
+            --         'extractVariable',
+            --         'extractConstant',
+            --         'extractVariableAllOccurrence',
+            --     },
+            -- },
         },
     },
 }
--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -69,6 +69,15 @@ local plugins = {
         end,
     },
 
+
+    -- Highlight todo, notes, etc in comments
+    {
+        'folke/todo-comments.nvim',
+        event = 'VimEnter',
+        dependencies = { 'nvim-lua/plenary.nvim' },
+        opts = { signs = false }
+    },
+
     {
         "tpope/vim-fugitive",
         cmd = "Git",
