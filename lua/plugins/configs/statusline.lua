10282c64018e4b4ed7b815312ab9e7ec398e9c2b
krolxon <krolyxon@tutanota.com>
2024-07-17 18:56:14 +0530

    remove lsp stuff from statusline

--- lua/plugins/configs/statusline.lua
+++ lua/plugins/configs/statusline.lua
@@ -1,42 +1,3 @@
-local function LSP_progress()
-  if not rawget(vim, "lsp") then
-    return ""
-  end
-
-  local Lsp = vim.lsp.util.get_progress_messages()[1]
-
-  if vim.o.columns < 120 or not Lsp then
-    return ""
-  end
-
-  local msg = Lsp.message or ""
-  local percentage = Lsp.percentage or 0
-  local title = Lsp.title or ""
-  local spinners = { "", "󰪞", "󰪟", "󰪠", "󰪢", "󰪣", "󰪤", "󰪥" }
-  local ms = vim.loop.hrtime() / 1000000
-  local frame = math.floor(ms / 120) % #spinners
-  local content = string.format(" %%<%s %s %s (%s%%%%) ", spinners[frame + 1], title, msg, percentage)
-
-  return ("%#St_LspProgress#" .. content) or ""
-end
-
-  -- Lsp server name .
-local function lsp_server()
-    local msg = 'lsp'
-    local buf_ft = vim.api.nvim_buf_get_option(0, 'filetype')
-    local clients = vim.lsp.get_active_clients()
-    if next(clients) == nil then
-      return msg
-    end
-    for _, client in ipairs(clients) do
-      local filetypes = client.config.filetypes
-      if filetypes and vim.fn.index(filetypes, buf_ft) ~= -1 then
-        return client.name
-      end
-    end
-    return msg
-  end
-
 require('lualine').setup {
     options = {
         icons_enabled = true,
@@ -60,10 +21,8 @@ require('lualine').setup {
         lualine_a = { 'mode' },
         lualine_b = { 'branch', 'diff', 'diagnostics' },
         lualine_c = {
-            { lsp_server,   icon = ' ', color = { fg = '#ffffff', gui = 'bold' } },
             { "filename", path = 1, symbols = { modified = "  ", readonly = "", unnamed = "" } }
         },
-        lualine_x = { LSP_progress, 'encoding', 'fileformat', 'filetype' },
         lualine_y = { 'progress' },
         lualine_z = { 'location' }
     },
